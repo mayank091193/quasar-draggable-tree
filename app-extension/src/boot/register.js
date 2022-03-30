dfbdf18bf06c6a2f5cc67fd6d3ee50a58872deb0
pratikpatel <pratikpatelpp802@gmail.com>
2022-03-30 16:28:21 +0530

    Initial Commit

--- app-extension/src/boot/register.js
+++ app-extension/src/boot/register.js
@@ -1,5 +1,5 @@
 import { boot } from 'quasar/wrappers'
-import * as VuePlugin from 'quasar-ui-qdraggabletree2/src/vue-plugin'
+import * as VuePlugin from 'quasar-ui-qdraggabletree/src/vue-plugin'
 
 export default boot(({ app }) => {
   app.use(VuePlugin)
