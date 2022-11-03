dffe27b301c5046e4e93e06ada8be7412221e92c
Pratik Patel <34883558+pratik227@users.noreply.github.com>
2022-11-03 14:53:49 +0530

    Added Vite Support

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -6,12 +6,14 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-function extendConf (conf) {
-  // register our boot file
-  conf.boot.push('~quasar-app-extension-qdraggabletree/src/boot/register.js')
+function extendConf(conf, api) {
+    // register our boot file
+    conf.boot.push('~quasar-app-extension-qdraggabletree2/src/boot/register.js')
 
-  // make sure app extension files & ui package gets transpiled
-  conf.build.transpileDependencies.push(/quasar-app-extension-qdraggabletree[\\/]src/)
+    // make sure app extension files & ui package gets transpiled
+    if (api.hasVite !== true) {
+        conf.build.transpileDependencies.push(/quasar-app-extension-qdraggabletree2[\\/]src/)
+    }
 }
 
 module.exports = function (api) {
@@ -19,7 +21,14 @@ module.exports = function (api) {
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of "@quasar/app" CLI
   api.compatibleWith('quasar', '^2.0.0')
-  api.compatibleWith('@quasar/app', '^3.0.0')
+
+  if (api.hasVite === true) {
+    api.compatibleWith('@quasar/app-vite', '^1.0.0-alpha.0')
+  }
+  else {
+    // should be "@quasar/app-webpack" but that is not backward compatible
+    api.compatibleWith('@quasar/app', '^3.0.0')
+  }
 
   // Uncomment the line below if you provide a JSON API for your component
   // api.registerDescribeApi('qdraggabletree', '~quasar-ui-ui/src/components/qdraggabletree.json')
